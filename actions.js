757aa211dd7008e53d70d9317a94a215b0e78aba
AlexK-99 <bazoo1q@gmail.com>
2020-03-19 23:29:58 +0100

    Fix Code Style

--- actions.js
+++ actions.js
@@ -1,92 +1,100 @@
 exports.getActions = function() {
-	return {
-		changeNdiSource: {
-			label: 'Change NDI Decode Source',
-			options: [{
-					type: 'dropdown',
-					label: 'Source',
-					id: 'source',
-					default: 'custom',
-					choices: this.ndi.getSourceDropdown()
-				},
-				{
-					type: 'textinput',
-					label: 'NDI Source Name',
-					id: 'ndiSource',
-					width: 12,
-					regex: this.REGEX_TEXT
-				},
-				{
-					type: 'textinput',
-					label: 'NDI Source IP',
-					id: 'ndiSourceIp',
-					width: 8,
-					default: "127.0.0.1",
-					regex: this.REGEX_IP,
-				},
-				{
-					type: 'number',
-					label: 'NDI Sourec Port',
-					id: 'ndiSourcePort',
-					width: 3,
-					regex: this.REGEX_PORT,
-					min: 1,
-					max: 65535,
-					default: 8080,
-				}
-			]
-		},
-		removeNdiSource: {
-			label: 'Remove NDI Decode Source from DB',
-			options: [{
-				type: 'dropdown',
-				label: 'Source',
-				id: 'source',
-				default: 'custom',
-				choices: this.ndi.getSourceDropdown()
-			}, ]
-		}
-	};
+  return {
+    changeNdiSource: {
+      label: 'Change NDI Decode Source',
+      options: [{
+          type: 'dropdown',
+          label: 'Source',
+          id: 'source',
+          default: 'custom',
+          choices: this.ndi.getSourceDropdown()
+        },
+        {
+          type: 'textinput',
+          label: 'NDI Source Name',
+          id: 'ndiSource',
+          width: 12,
+          regex: this.REGEX_TEXT
+        },
+        {
+          type: 'textinput',
+          label: 'NDI Source IP',
+          id: 'ndiSourceIp',
+          width: 8,
+          default: "127.0.0.1",
+          regex: this.REGEX_IP,
+        },
+        {
+          type: 'number',
+          label: 'NDI Sourec Port',
+          id: 'ndiSourcePort',
+          width: 3,
+          regex: this.REGEX_PORT,
+          min: 1,
+          max: 65535,
+          default: 8080,
+        }
+      ]
+    },
+    removeNdiSource: {
+      label: 'Remove NDI Decode Source from DB',
+      options: [{
+        type: 'dropdown',
+        label: 'Source',
+        id: 'source',
+        default: 'custom',
+        choices: this.ndi.getSourceDropdown()
+      }, ]
+    }
+  };
 };
 
 exports.executeAction = function(action) {
-	if (action.action === 'changeNdiSource') {
-		if (action.options.source == 'custom') {
-			if (action.options.ndiSource && action.options.ndiSourceIp && action.options.ndiSourcePort) {
-				this.api.setNdiDecodeSource(action.options.ndiSourceIp, action.options.ndiSourcePort, action.options.ndiSource);
-			} else {
-				this.log('error', 'The Ndi decoding source could not be changed!!! <h4>Check Action Config!!!</h4>');
-			}
-		} else {
-			if (action.options.source) {
-				(async () => {
-					var sourceSize = await this.ndi.getDb().get('source').filter({ name: action.options.source }).size().value();
-					if (sourceSize > 0) {
-						var sourceDb = await this.ndi.getDb().get('source').filter({ name: action.options.source }).value();
-						var urlAddressSplit = sourceDb[0].urlAddress.split(':');
+  if (action.action === 'changeNdiSource') {
+    if (action.options.source == 'custom') {
+      if (action.options.ndiSource && action.options.ndiSourceIp && action.options.ndiSourcePort) {
+        this.api.setNdiDecodeSource(action.options.ndiSourceIp, action.options.ndiSourcePort, action.options.ndiSource);
+      } else {
+        this.log('error', 'The Ndi decoding source could not be changed!!! <h4>Check Action Config!!!</h4>');
+      }
+    } else {
+      if (action.options.source) {
+        (async () => {
+          var sourceSize = await this.ndi.getDb().get('source').filter({
+            name: action.options.source
+          }).size().value();
+          if (sourceSize > 0) {
+            var sourceDb = await this.ndi.getDb().get('source').filter({
+              name: action.options.source
+            }).value();
+            var urlAddressSplit = sourceDb[0].urlAddress.split(':');
 
-						this.api.setNdiDecodeSource(urlAddressSplit[0], urlAddressSplit[1], sourceDb[0].name);
-					} else {
-						this.log('error', 'The Ndi decoding source could not be changed!!! <h4>Check Action Config!!!</h4>');
-					}
-				})();
-			} else {
-				this.log('error', 'The Ndi decoding source could not be changed!!! <h4>Check Action Config!!!</h4>');
-			}
-		}
-	} else if (action.action === 'removeNdiSource') {
-		if (action.options.source) {
-			(async () => {
-				var sourceSize = await this.ndi.getDb().get('source').filter({ name: action.options.source }).size().value();
-				if (sourceSize > 0) {
-					await this.ndi.getDb().get('source').remove({ name: action.options.source }).write();
-					this.log('info', `Remove NDI Decode Source: ${action.options.source}!!!`);
-				} else {
-					this.log('error', 'The Ndi decoding source could not be removed!!! <h4>Check Action Config!!!</h4>');
-				}
-			})();
-		} else {
-			this.log('error', 'The Ndi decoding source could not be removed!!! <h4>Check Action Config!!!</h4>');
-		}
-	}
+            this.api.setNdiDecodeSource(urlAddressSplit[0], urlAddressSplit[1], sourceDb[0].name);
+          } else {
+            this.log('error', 'The Ndi decoding source could not be changed!!! <h4>Check Action Config!!!</h4>');
+          }
+        })();
+      } else {
+        this.log('error', 'The Ndi decoding source could not be changed!!! <h4>Check Action Config!!!</h4>');
+      }
+    }
+  } else if (action.action === 'removeNdiSource') {
+    if (action.options.source) {
+      (async () => {
+        var sourceSize = await this.ndi.getDb().get('source').filter({
+          name: action.options.source
+        }).size().value();
+        if (sourceSize > 0) {
+          await this.ndi.getDb().get('source').remove({
+            name: action.options.source
+          }).write();
+          this.log('info', `Remove NDI Decode Source: ${action.options.source}!!!`);
+        } else {
+          this.log('error', 'The Ndi decoding source could not be removed!!! <h4>Check Action Config!!!</h4>');
+        }
+      })();
+    } else {
+      this.log('error', 'The Ndi decoding source could not be removed!!! <h4>Check Action Config!!!</h4>');
+    }
+  }
 };
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const instance_skel = require('../../instance_skel');
+const instance_skel       										= require('../../instance_skel');
+const { getConfigFields } 										= require('./config');
 
-const { getConfigFields } = require('./config');
-
-const instance_api = require('./birddogapi');
-const ndi_api = require('./ndiapi');
+const instance_api        										= require('./birddogapi');
+const ndi_api             										= require('./ndiapi');
 
 const { executeAction, getActions } = require('./actions');
 
@@ -11,56 +10,56 @@ var log;
 
 class BirdDogInstance extends instance_skel {
 
-	constructor(system, id, config) {
+  constructor(system, id, config) {
 
-		super(system, id, config);
+    super(system, id, config);
 
-		this.api = new instance_api(this);
-		this.ndi = new ndi_api(this);
-		this.getActions = getActions;
+    this.api = new instance_api(this);
+    this.ndi = new ndi_api(this);
+    this.getActions = getActions;
 
-	}
+  }
 
-	init() {
+  init() {
 
-		this.status(this.STATUS_UNKNOWN);
+    this.status(this.STATUS_UNKNOWN);
 
-		if (this.config.nsdMode) {
-			this.ndi.startNdiSourceInterval();
-		}
-		this.api.aboutDevice();
-		this.actions();
-	}
+    if (this.config.nsdMode) {
+      this.ndi.startNdiSourceInterval();
+    }
+    this.api.aboutDevice();
+    this.actions();
+  }
 
-	updateConfig(config) {
-		if (this.config.nsdMode != config.nsdMode) {
-			if (config.nsdMode) {
-				this.ndi.startNdiSourceInterval();
-			} else {
-				this.ndi.stopNdiSourceInterval();
-			}
-		}
+  updateConfig(config) {
+    if (this.config.nsdMode != config.nsdMode) {
+      if (config.nsdMode) {
+        this.ndi.startNdiSourceInterval();
+      } else {
+        this.ndi.stopNdiSourceInterval();
+      }
+    }
 
-		this.config = config;
+    this.config = config;
 
-		this.actions();
-	}
+    this.actions();
+  }
 
-	config_fields() {
-		return getConfigFields();
-	}
+  config_fields() {
+    return getConfigFields();
+  }
 
-	destroy() {
-		this.debug("destroy", this.id);
-	}
+  destroy() {
+    this.debug("destroy", this.id);
+  }
 
-	actions() {
-		this.system.emit('instance_actions', this.id, getActions.bind(this)());
-	}
+  actions() {
+    this.system.emit('instance_actions', this.id, getActions.bind(this)());
+  }
 
-	action(action) {
-		executeAction.bind(this)(action);
-	}
+  action(action) {
+    executeAction.bind(this)(action);
+  }
 
 }
 module.exports = BirdDogInstance;
