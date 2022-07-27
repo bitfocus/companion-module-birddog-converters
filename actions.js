376909bb9a260a683717d11575b9867f4a972e1f
enawaves <33061396+enawaves@users.noreply.github.com>
2022-07-27 11:20:41 -0300

    Add files via upload

--- actions.js
+++ actions.js
@@ -1,3 +1,6 @@
+let sourceIndex = 0
+
+
 exports.getActions = function () {
 	return {
 		changeNDISource: {
@@ -48,10 +51,29 @@ exports.getActions = function () {
 		refreshNDISourceList: {
 			label: 'Refresh NDI Source List',
 		},
+		sicleTroughSources: {
+			label: 'Sicle Trough Sources',
+			options: [
+				{
+					 type: 'dropdown',
+					 label: 'Direction',
+					 id: 'direction',
+					 default: 1,
+					 choices: [
+					   { id: 1, label: 'Up' },
+					   { id: -1, label: 'Down' }
+					 ]
+}
+			]
+		},
 	}
 }
 
+
 exports.executeAction = function (action) {
+	
+
+
 	if (action.action === 'changeNDISource') {
 		if (action.options.source != undefined && action.options.source != 'No sources found') {
 			let urlSplit = this.api.sourceList[action.options.source].split(':')
@@ -59,6 +81,15 @@ exports.executeAction = function (action) {
 			let port = urlSplit[1]
 			let name = action.options.source
 			this.api.setNDIDecodeSource(ip, port, name)
+			
+			let sourceList = this.api.sourceList
+
+			sourceIndex = sourceList.findIndex((item) => {
+			  return item.id === name
+			})
+			
+			this.log('error','sourceIndex = ' + sourceIndex)
+
 		} else {
 			this.log(
 				'error',
@@ -76,5 +107,49 @@ exports.executeAction = function (action) {
 		}
 	} else if (action.action === 'refreshNDISourceList') {
 		this.api.getSourceList()
+	}else if (action.action === 'sicleTroughSources') {
+		//this.log('error', 'currentSource = ' + this.api.currentSource)
+
+		
+		sourceIndex = sourceIndex + action.options.direction
+
+		if (sourceIndex <= -1){
+			this.log('error','Pase al Ultimo')
+			sourceIndex = this.api.sourceList.length - 1
+		}else if (sourceIndex >= this.api.sourceList.length){
+			this.log('error','Pase al Primero')
+			sourceIndex = 0
+		}
+
+
+		this.log('error','sourceIndex = ' + sourceIndex)
+		
+		
+
+		let sourceList = this.api.sourceList
+		this.log('error','PASE sourceList')
+
+		
+		
+		let selectedSourceID = sourceList[sourceIndex].id
+		this.log('error', 'selectedSourceID =' + selectedSourceID)
+		
+		let selectedSource = sourceList[selectedSourceID]
+		this.log('error', 'selectedSource =' + selectedSource)
+		
+		
+		if (selectedSource != undefined) {
+			let urlSplit = selectedSource.split(':')
+			let ip = urlSplit[0]
+			let port = urlSplit[1]
+			let name = selectedSourceID
+			this.api.setNDIDecodeSource(ip, port, name)
+			this.log('error','------Setie el source------')
+		} else {
+			this.log(
+				'error',
+				'Unable to find the configured NDI source. Please check the NDI source info in the action configuration'
+			)
+		}
 	}
 }
